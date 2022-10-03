891f85550328fb07767833bf00cf37a96848b9e0
paduh <perfect.aduh@gmail.com>
2022-10-03 09:42:19 +0100

    Implement WeatherForcastViewModel complete

---
+++ iOSCleanArchitectureWeatherApp/WeatherForcastUI/ViewModel/WeatherForcastViewModel.swift
@@ -0,0 +1,88 @@
+//
+//  WeatherForcastViewModel.swift
+//  iOSCleanArchitectureWeatherApp
+//
+//  Created by Perfect Aduh on 02/10/2022.
+//  Copyright © 2022 Perfect Aduh. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - WeatherForcastViewModelDelegate
+
+protocol WeatherForcastViewModelDelegate: AnyObject {
+    var onLoadingStateChange: ((Bool) -> Void)? { get set }
+    var onCurrentDayWeatherForcastLoad: ((CurrentDayWeatherForcast) -> Void)? { get set }
+    var onFiveDaysWeatherForcastLoad: (([FiveDaysWeatherForcast]) -> Void)? { get set }
+    var onError: ((String) -> Void)? { get set }
+
+    func loadWeatherForcast()
+    func viewDidLoad()
+}
+
+// MARK: - WeatherForcastViewModel
+
+final class WeatherForcastViewModel: WeatherForcastViewModelDelegate {
+    typealias Observer<T> = (T) -> Void
+
+    var onLoadingStateChange: Observer<Bool>?
+    var onCurrentDayWeatherForcastLoad: Observer<CurrentDayWeatherForcast>?
+    var onFiveDaysWeatherForcastLoad: Observer<[FiveDaysWeatherForcast]>?
+    var onError: Observer<String>?
+
+    private let currentDayWeatherForcastLoader: CurrentDayWeatherForcastLoader
+    private let fiveDaysWeatherForcastLoader: FiveDaysWeatherForcastLoader
+
+    private let dispatchGroup = DispatchGroup()
+
+    init(
+        currentDayWeatherForcastLoader: CurrentDayWeatherForcastLoader,
+        fiveDaysWeatherForcastLoader: FiveDaysWeatherForcastLoader) {
+        self.currentDayWeatherForcastLoader = currentDayWeatherForcastLoader
+        self.fiveDaysWeatherForcastLoader = fiveDaysWeatherForcastLoader
+    }
+
+    func viewDidLoad() {
+        onLoadingStateChange?(true)
+        dispatchGroup.notify(queue: .main) { [ weak self] in
+            self?.onLoadingStateChange?(false)
+         }
+    }
+
+    func loadWeatherForcast() {
+
+    }
+
+    private func loadCurrentDayWeatherForcast() {
+        dispatchGroup.enter()
+        currentDayWeatherForcastLoader.load { [weak self] result in
+            guard let self = self else { return }
+
+            self.dispatchGroup.leave()
+
+            switch result {
+            case .failure(let error):
+                self.onError?(error.localizedDescription)
+            case .success(let weatherForcast):
+                self.onCurrentDayWeatherForcastLoad?(weatherForcast)
+            }
+        }
+    }
+
+    private func loadFiveDaysWeatherForcast() {
+        dispatchGroup.enter()
+
+        fiveDaysWeatherForcastLoader.load { [weak self] result in
+            guard let self = self else { return }
+
+            self.dispatchGroup.leave()
+
+            switch result {
+            case .failure(let error):
+                self.onError?(error.localizedDescription)
+            case .success(let weatherForcast):
+                self.onFiveDaysWeatherForcastLoad?(weatherForcast)
+            }
+        }
+    }
+}
